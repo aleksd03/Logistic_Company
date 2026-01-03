95f5d36e2ab4bc311c3b3264b0bf2ba6eda8f275
aleksd03 <aleks.d061003@gmail.com>
2026-01-03 17:28:07 +0200

    Lombok and BaseEntity

---
+++ src/main/java/org/informatics/entity/BaseEntity.java
@@ -0,0 +1,14 @@
+package org.informatics.entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}
