06c7560ba9a6dcc76ec043d7d9558e0a3e5b8da8
ZackFreiday <mutafchiev4000@gmail.com>
2026-01-04 03:35:54 +0200

    Update SessionFactoryUtil.java

--- src/main/java/org/informatics/configuration/SessionFactoryUtil.java
+++ src/main/java/org/informatics/configuration/SessionFactoryUtil.java
@@ -4,6 +4,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
+
+// Entities
 import org.informatics.entity.User;
 import org.informatics.entity.Client;
 import org.informatics.entity.Employee;
@@ -12,17 +14,32 @@ import org.informatics.entity.Office;
 import org.informatics.entity.Shipment;
 
 public class SessionFactoryUtil {
+
     private static SessionFactory sessionFactory;
 
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
+
             Configuration configuration = new Configuration();
+
+            // ---- REGISTER ALL ENTITIES USED IN THE PROJECT ----
             configuration.addAnnotatedClass(User.class);
-            ServiceRegistry serviceRegistry
-                    = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
+            configuration.addAnnotatedClass(Client.class);
+            configuration.addAnnotatedClass(Employee.class);
+            configuration.addAnnotatedClass(Company.class);
+            configuration.addAnnotatedClass(Office.class);
+            configuration.addAnnotatedClass(Shipment.class);
+
+            // ---- BUILD SESSION FACTORY ----
+            ServiceRegistry serviceRegistry =
+                    new StandardServiceRegistryBuilder()
+                            .applySettings(configuration.getProperties())
+                            .build();
+
             sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         }
+
         return sessionFactory;
     }
 }
+
